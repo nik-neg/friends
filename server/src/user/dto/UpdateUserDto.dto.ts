de901dd39ad54735018a9781bff6071d666939ee
Alex Genkin <alex.genkin.13@gmail.com>
2024-02-16 20:35:25 +0200

    feat: endpoints

--- server/src/user/dto/UpdateUserDto.dto.ts
+++ server/src/user/dto/UpdateUserDto.dto.ts
@@ -1,6 +1,6 @@
 import { PartialType } from '@nestjs/mapped-types';
-import { CreateUserDto } from './create-user.dto';
 import { IsNotEmpty, IsOptional, IsString } from 'class-validator';
+import { CreateUserDto } from './CreateUserDto.dto';
 
 export class UpdateUserDto extends PartialType(CreateUserDto) {
 
--- server/src/user/dto/transforms/stringToNumber.transform.ts
+++ server/src/user/dto/transforms/stringToNumber.transform.ts
@@ -1,4 +1,3 @@
 export const stringToNumber = ({ value, key, obj, type }) => {
-  console.log('value', value);
   return parseInt(value, 10);
 };
