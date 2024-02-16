42e7a297218623ec80c99ac66e2d0d3fa75fcd88
Alex Genkin <alex.genkin.13@gmail.com>
2024-02-16 20:48:30 +0200

    feat: endpoints

--- server/src/auth/dto/RegisterDto.dto.ts
+++ server/src/auth/dto/RegisterDto.dto.ts
@@ -16,11 +16,4 @@ export class RegisterDto {
   @Length(6, 20)
   password: string;
 
-  // constructor(data: RegisterDto) {
-  //   this.first_name = data.first_name;
-  //   this.last_name = data.last_name;
-  //   this.email = data.email;
-  //   this.password = data.password;
-  // }
-
 }
