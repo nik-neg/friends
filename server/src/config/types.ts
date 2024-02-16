7dd3a0c7a5816f9e945345d9240ef03337eab5f0
Alex Genkin <alex.genkin.13@gmail.com>
2024-02-16 17:45:17 +0200

    feat: db setup

---
+++ server/src/config/types.ts
@@ -0,0 +1,10 @@
+export interface ConfigGetOptions {
+
+  HOST: string;
+  PORT: number;
+  DATABASE_USERNAME: string;
+  DATABASE_PASSWORD: string;
+  DATABASE: string;
+  JWT_EXPIRATION: number;
+
+}
--- server/src/main.ts
+++ server/src/main.ts
@@ -10,7 +10,7 @@ async function bootstrap() {
   const configService = app.get(ConfigService);
 
   const port = configService.get<number>('PORT', 3000);
-
   await app.listen(port);
 }
+
 bootstrap();
